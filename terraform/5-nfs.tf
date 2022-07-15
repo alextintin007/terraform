7c16f6b6b72208414e184c2b6071b7630a13908f
Xavier Tintin <79267255+alextintin007@users.noreply.github.com>
2022-07-15 18:18:20 -0500

    adding pv-pod

--- terraform/5-nfs.tf
+++ terraform/5-nfs.tf
@@ -122,3 +122,31 @@ resource "kubernetes_persistent_volume_claim" "nfs_pvc" {
     volume_name = kubernetes_persistent_volume.nfs_pv.metadata.0.name
   }
 }
+
+//  pv-pod
+resource "kubernetes_pod" "pv_pod" {
+  metadata {
+    name      = "pv-pod"
+    namespace = kubernetes_namespace.namespace1.metadata.0.name
+  }
+  spec {
+    volume {
+      name = "task-pv-storage"
+      persistent_volume_claim {
+        claim_name = "nfs-${var.name}"
+      }
+    }
+    container {
+      name    = "pv-container"
+      image   = "busybox"
+      command = ["tail", "-f", "/dev/null"]
+      volume_mount {
+        name       = "task-pv-storage"
+        mount_path = "/mnt/data"
+      }
+    }
+  }
+  depends_on = [
+    google_container_node_pool.npool1
+  ]
+}
